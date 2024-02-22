35c739ad52f00b434d23c06ce801475b2e913e39
SergejSkiba <sergej.skiba1981@gmail.com>
2024-02-22 20:03:17 +0200

    meat: lesson: string concatenation

---
+++ book_test2.2.py
@@ -0,0 +1,12 @@
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+print(name.title())
+first_name = "ada"
+last_name = "lowelace"
+full_name = first_name + " " + last_name
+message = "hello, " + full_name.title() + "!"
+print(message)
+Python = "studentas"
+print("Python")
+print("\tPython")
