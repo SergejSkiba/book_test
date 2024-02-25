7e9d9ef75afaa9f76966b9c9d5c209e668f14c03
SergejSkiba <sergej.skiba1981@gmail.com>
2024-02-25 03:46:32 +0200

    feat: use of pop and remove removal programmes

--- book_test.py
+++ book_test.py
@@ -48,3 +48,21 @@ del motorcycles[1]
 print(motorcycles)
 del motorcycles[-1]
 print(motorcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop()
+print('The first motorcycle I owned was a ' + first_owned.title() + '.')
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
