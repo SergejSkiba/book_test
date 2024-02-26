cf3825ed82689ac7d045f44cbf928ad7b23e416c
SergejSkiba <sergej.skiba1981@gmail.com>
2024-02-26 02:14:25 +0200

    ДобавлауÑfeat: lesson 2 from the book

---
+++ name_cases.py
@@ -0,0 +1,15 @@
+name = "sergej"
+print("Hello, " + name.title() + " would you like to learn some Python today?")
+print(name.lower())
+print(name.upper())
+print(name.title())
+message = '"It takes discipline, willpower and character to succeed."'
+author = "Sergej Skiba once said, "
+print(author + message)
+famous_persone = "Albert Einstein - "
+message = '"Strive not for success, but for the value it provides"'
+print(famous_persone + message)
+name = "  Sergej Skiba  "
+print(name.strip() + "\n\tdiscipline\n\twillpower\n\tcharacter")
+print(name.rstrip())
+print(name.lstrip())
